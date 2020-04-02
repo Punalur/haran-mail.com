12ca6e997ecafa6b45dfc86075469cd28e6fd196
Punalur <haran@mail.com>
2020-04-02 15:14:14 -0400

    Update createData.php

--- createData.php
+++ createData.php
@@ -2,11 +2,13 @@
 
             function createRecord()
             {
+            // Store each database info in variable
                 $servername = "localhost";
                 $username = "root";
                 $password = "";
                 $database = "movieflix_database";
 
+            // Connect to database
                 $con = mysqli_connect($servername, $username, $password, $database);
 
                 if(!$con)
@@ -17,13 +19,16 @@
                 {
                     echo("Success!");
                 }
-
+            
+            // Store user input in variable
                 $moviename = $_POST["movie-name"];
                 $moviegenre = $_POST["movie-genre"];
                 $moviedirector = $_POST["movie-director"];
 
+            // Insert data to database
                 $sql = "INSERT INTO movieflix_table (Title, Genre, Director) VALUES('$moviename', '$moviegenre', '$moviedirector')";
-
+            
+                // Run the query
                 if(mysqli_query($con, $sql))
                 {
                     echo("Your entry will be verified and we will get back to you soon");
@@ -32,9 +37,11 @@
                 {
                     echo("Please check you connection : ". mysqli_connect_error($con));
                 }
-
+            
+            //Close the connection
                 mysqli_close($con);
-
+            
+            //Redirect to index page
                 header('location: index.php');
 
             }
