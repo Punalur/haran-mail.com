a00f77a8897c28d276ca9b46e3a17e23af56d8c2
Punalur <haran@mail.com>
2020-04-02 15:38:29 -0400

    Update update.php

--- update.php
+++ update.php
@@ -1,34 +1,26 @@
 <?php
-/*
-    isset to call a function
-    create a connection to the database
-    store user input into variables
-    setup INSERT query to run
-    close the connection
-    redirect user to index.php
-*/
 
 function insertInfo()
 {
-    //store database conection in variables
+//store database conection in variables
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "movieflix_database";
 
-    //store connection in one variable
+ //store connection in one variable
     $connection = mysqli_connect($servername, $username, $password, $database);
 
-    //store user input in variables
+ //store user input in variables
     $movieID = $_POST["update-ID"];
     $movieName = $_POST["movie-name"];
     $movieGenre = $_POST["movie-genre"];
     $movieDirector = $_POST["movie-director"];
 
-    //insert query
+ //insert query
     $sql = "UPDATE movieflix_table SET Title='$movieName', Genre='$movieGenre', Director='$movieDirector' WHERE ID='$movieID'";
 
-    //Plug in the connection and query
+ //Plug in the connection and query
     $updateQuery = mysqli_query($connection, $sql);
 
     if(!$updateQuery)
@@ -40,10 +32,10 @@ function insertInfo()
             echo("Updated successfully!");
         }
 
-    //close the connection
+ //close the connection
     mysqli_close($connection);
 
-    //redirect the page
+ //redirect the page
     header("location: index.php");
 }
 
