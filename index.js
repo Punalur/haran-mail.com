ce2df87b28e1ef0f91c4ef4111e69a7671a94152
Punalur <haran@mail.com>
2020-04-02 15:15:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,3 @@ import react from "react"
 import ReactDOM from "react-dom"
 
 ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
-
-/*
-
-I know this will happen. As I am quite flexible, I used to renew for few of my friends even before they pay which is not at all quite common.
-To avoid this kind of issues with few clients, I activate their account only after receiving the cash.
-
-If you want, I could do same with you too. People usually ask to activate and they say will pay later.
-But after several days and weeks of follow-up, they pay very late and unfortunately, they start counting from that payment date.
-Delibrately or by mistake they forget I activated their account before.
-
-So to avoid, this I regularly keep account of all transaction and activation.
-
-*/
